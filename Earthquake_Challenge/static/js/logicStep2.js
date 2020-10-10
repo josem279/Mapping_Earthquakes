b04234807094d7ad625e41d7aa426bed80f906fd
josem279 <martinez.jose279@gmail.com>
2020-10-10 12:49:52 -0400

    Added major earthquake data

--- Earthquake_Challenge/static/js/logicStep2.js
+++ Earthquake_Challenge/static/js/logicStep2.js
@@ -62,8 +62,8 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
                 console.log(data);
                 return L.circleMarker(latlng);
             },
-          // We set the style for each circleMarker using our styleInfo function.
+        // We set the style for each circleMarker using our styleInfo function.
         style: styleInfo
-        }).addTo(map);
+    }).addTo(map);
 });
 
